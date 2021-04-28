2ecfa855a4bca07e49c65ed30019ff5d0dc96a6d
calvix1 <santiago.d.calvo@gmail.com>
2021-04-27 21:37:47 -0300

    repo init

---
+++ scripts/achievement.js
@@ -0,0 +1,206 @@
+/*function achievementUnlocked(title, description) {
+    var hasClass = $('.ach').hasClass('achieved');
+    if (hasClass) return;
+    $('.title').html(title);
+    $('.detail').html(description);
+    $('.ach').addClass("achieved");
+    setTimeout(function() {
+        $('.ach').removeClass("achieved");
+    }, 5000)
+}*/
+var achievModalFillDone = false; //flag for filling modal achievement
+
+function achievementUnlocked(title, description) {
+
+    let achievContainer = document.getElementById("achievementContainer");
+    const toast = document.createElement("div");
+    toast.innerHTML = `<div class="ach ">
+    <div class="icon"><i class="fa fa-flag" aria-hidden="true"></i></div>
+    <div class="text_wrap fw-bold">
+        <p class="title">${title}</p>
+        <span class="detail">${description}</span>
+    </div>
+</div>`
+
+    let delay = achievContainer.childNodes.length * 100;
+
+    setTimeout(function() {
+        toast.childNodes[0].classList.add("achieved")
+    }, delay)
+
+    achievContainer.appendChild(toast);
+
+    setTimeout(function() {
+        toast.remove();
+    }, 5000 + delay)
+}
+
+/* Gold achievements */
+
+/* 10 gold */
+
+function poorMan() {
+
+    if (!clicker.achievements.poorMan.unlocked && clicker.gold >= 10) {
+
+        clicker.achievements.poorMan.unlocked = true;
+        clicker.achievementsUnlocked++;
+        achievementUnlocked(clicker.achievements.poorMan.title, clicker.achievements.poorMan.description);
+    }
+}
+
+function stillKindaPoor() {
+
+    if (!clicker.achievements.stillKindaPoor.unlocked && clicker.gold >= 100) {
+
+        clicker.achievements.stillKindaPoor.unlocked = true;
+        clicker.achievementsUnlocked++;
+        achievementUnlocked(clicker.achievements.stillKindaPoor.title, clicker.achievements.stillKindaPoor.description);
+    }
+}
+
+function gettingBy() {
+
+    if (!clicker.achievements.gettingBy.unlocked && clicker.gold >= 1000) {
+
+        clicker.achievements.gettingBy.unlocked = true;
+        clicker.achievementsUnlocked++;
+        achievementUnlocked(clicker.achievements.gettingBy.title, clicker.achievements.gettingBy.description);
+    }
+}
+
+function noobMiner() {
+
+    if (!clicker.achievements.noobMiner.unlocked && clicker.upgrades.upgradeOne.amount >= 1) {
+
+        clicker.achievements.noobMiner.unlocked = true;
+        clicker.achievementsUnlocked++;
+        achievementUnlocked(clicker.achievements.noobMiner.title, clicker.achievements.noobMiner.description);
+    }
+}
+
+function amateurMiner() {
+
+    if (!clicker.achievements.amateurMiner.unlocked && clicker.upgrades.upgradeTwo.amount >= 1) {
+
+        clicker.achievements.amateurMiner.unlocked = true;
+        clicker.achievementsUnlocked++;
+        achievementUnlocked(clicker.achievements.amateurMiner.title, clicker.achievements.amateurMiner.description);
+    }
+}
+
+function Miner() {
+
+    if (!clicker.achievements.Miner.unlocked && clicker.upgrades.upgradeThree.amount >= 1) {
+
+        clicker.achievements.Miner.unlocked = true;
+        clicker.achievementsUnlocked++;
+        achievementUnlocked(clicker.achievements.Miner.title, clicker.achievements.Miner.description);
+    }
+}
+
+
+function achDebbug() {
+
+    achievementUnlocked("This is a debbug achievement", "Weird achievement to get if you ask me...");
+    achievementUnlocked("This is a debbug achievement", "Weird achievement to get if you ask me...");
+    achievementUnlocked("This is a debbug achievement", "Weird achievement to get if you ask me...");
+    achievementUnlocked("This is a debbug achievement", "Weird achievement to get if you ask me...");
+    achievementUnlocked("This is a debbug achievement", "Weird achievement to get if you ask me...");
+    achievementUnlocked("This is a debbug achievement", "Weird achievement to get if you ask me...");
+    achievementUnlocked("This is a debbug achievement", "Weird achievement to get if you ask me...");
+
+}
+
+
+/* Update Function */
+
+function checkAchievements() {
+
+    poorMan();
+    stillKindaPoor();
+    gettingBy();
+    noobMiner();
+    amateurMiner();
+    Miner();
+}
+
+/* Achievement Modal */
+
+function fillAchievModal() {
+
+    var achievModal = document.getElementById("achievContainer");
+    if (!achievModalFillDone) {
+
+        for (i in clicker.achievements) {
+
+            const achiev = document.createElement("div");
+            achiev.classList.add("col-4", "d-flex", "justify-content-center", "p-0")
+            if (clicker.achievements[i].unlocked === true) {
+
+                achiev.innerHTML = `
+                <div class="achModal">
+                <div class="icon">${clicker.achievements[i].icon}</div>
+                <div class="text_wrap fw-bold">
+                    <p class="title">${clicker.achievements[i].title}</p>
+                    <span class="detail">${clicker.achievements[i].feat}</span>
+                </div>
+                </div>`
+            } else {
+
+                achiev.innerHTML = `
+                <div class="achModal">
+                <div class="icon"><i class="fas fa-lock"></i></i></div>
+                <div class="text_wrap fw-bold">
+                    <p class="title ">Achievement Locked</p>
+                    <span class="detail">You haven't got this Achievement yet!</span>
+                </div>
+                </div>`
+            }
+
+            achievModal.appendChild(achiev);
+        }
+
+        achievModalFillDone = true;
+    }
+}
+
+function emptyAchievModal() {
+
+    var achievModal = document.getElementById("achievContainer");
+
+    while (achievModal.firstChild) {
+        achievModal.removeChild(achievModal.lastChild);
+    }
+
+    achievModalFillDone = false;
+}
+
+function showAchievModal() {
+
+    fillAchievModal();
+    const achievCover = document.getElementById("achievModalCover");
+    const achievModal = document.getElementById("achievModal");
+    achievModal.classList.add("show-modal");
+    achievCover.classList.add("cover");
+}
+
+function closeAchievModal() {
+
+    const achievCover = document.getElementById("achievModalCover");
+    const achievModal = document.getElementById("achievModal");
+    achievModal.classList.remove("show-modal");
+    achievCover.classList.remove("cover");
+    emptyAchievModal();
+}
+
+/*document.addEventListener(
+    "click",
+    function(event) {
+        // If user either clicks X button OR clicks outside the modal window, then close modal by calling closeModal()
+        if (!event.target.closest(".achiev-modal-container")) {
+            closeAchievModal()
+        }
+    },
+    false
+)*/
---
+++ scripts/index.js
@@ -0,0 +1,236 @@
+let goldElem = document.getElementById('gold'),
+    goldPerSecondElem = document.getElementById('goldPerSecond'),
+    mineElem = document.getElementById('mine'),
+    pickaxeElem = document.getElementById('pickaxe'),
+    delay = 0;
+
+function createClickerDefault() {
+
+    return {
+        gold: 0,
+        goldPerSecond: 0,
+        achievementsUnlocked: 0,
+
+        upgrades: {
+
+            upgradeOne: {
+
+                amount: 0,
+                price: 10,
+                priceGrowth: 1.15,
+                gps: 1 / 20,
+                /* updates 20 times per second*/
+                name: "Copper Mine",
+                mineOre: "Copper",
+                mineChance: 0.001,
+                unlocked: false,
+                domRef: "upgradeOne",
+            },
+            upgradeTwo: {
+
+                amount: 0,
+                price: 100,
+                priceGrowth: 1.2,
+                gps: 10 / 20,
+                /* updates 20 times per second*/
+                name: "Silver Mine",
+                mineOre: "Silver",
+                mineChance: 0.001,
+                unlocked: false,
+                domRef: "upgradeTwo",
+            },
+            upgradeThree: {
+
+                amount: 0,
+                price: 1000,
+                priceGrowth: 1.25,
+                gps: 100 / 20,
+                /* updates 20 times per second*/
+                name: "Gold Mine",
+                mineOre: "Gold",
+                mineChance: 0.001,
+                unlocked: false,
+                domRef: "upgradeThree",
+            },
+        },
+        achievements: {
+
+            poorMan: {
+
+                unlocked: false,
+                title: "Poor Man",
+                description: "Nice! you got your first ten coins",
+                feat: "Getting ten coins",
+                icon: '<i class="far fa-money-bill-alt"></i>',
+            },
+            stillKindaPoor: {
+
+                unlocked: false,
+                title: "Still Kinda Poor",
+                description: "Great! but you're still kinda poor tough",
+                feat: "Getting one hundred coins",
+                icon: '<i class="far fa-money-bill-alt"></i>',
+            },
+            gettingBy: {
+
+                unlocked: false,
+                title: "Getting By",
+                description: "Finally! Enough money to get a dishwasher!",
+                feat: "Getting one thousand coins",
+                icon: '<i class="far fa-money-bill-alt"></i>',
+            },
+            noobMiner: {
+
+                unlocked: false,
+                title: "Noob Miner",
+                description: "Gotta try harder if you want to impress me...",
+                feat: "Getting your first Copper mine",
+                icon: '<i class="fas fa-hard-hat"></i>',
+            },
+            amateurMiner: {
+
+                unlocked: false,
+                title: "Amateur Miner",
+                description: "You are getting better at this mining thing",
+                feat: "Getting your first Silver mine",
+                icon: '<i class="fas fa-hard-hat"></i>',
+            },
+            Miner: {
+
+                unlocked: false,
+                title: "Miner",
+                description: "Cool! Now you are a proper miner",
+                feat: "Getting your first Gold mine",
+                icon: '<i class="fas fa-hard-hat"></i>',
+            }
+        },
+    }
+}
+
+let clicker = createClickerDefault();
+
+
+function pickaxeRotate() {
+
+    pickaxeElem.classList.remove('button-pickaxe-rotate');
+    setTimeout(() => pickaxeElem.classList.add('button-pickaxe-rotate'));
+}
+
+function mine() {
+
+    ++clicker.gold;
+    pickaxeRotate();
+}
+
+function goldUpdate() {
+
+    for (i in clicker.upgrades) {
+
+        clicker.gold += clicker.upgrades[i].amount * clicker.upgrades[i].gps;
+    }
+
+    goldElem.innerHTML = numberformat.format(Number(String(clicker.gold).split(".")[0]));
+}
+
+function updateUI(id) {
+
+    document.getElementById("goldPerSecond").innerHTML = clicker.goldPerSecond;
+    document.getElementById(id + 'Name').innerHTML = clicker.upgrades[id].name;
+    document.getElementById(id + 'Gps').innerHTML = "Gold per second: " + clicker.upgrades[id].gps * 20;
+    document.getElementById(id + 'MineChance').innerHTML = clicker.upgrades[id].mineOre + " mine chance: " + clicker.upgrades[id].mineChance + "%";
+    document.getElementById(id + 'Amount').innerHTML = "Amount: " + clicker.upgrades[id].amount;
+    document.getElementById(id + 'Price').innerHTML = "Price: " + numberformat.formatShort(Number(clicker.upgrades[id].price));
+}
+
+function buy(id) {
+
+    if (clicker.gold >= clicker.upgrades[id].price) {
+
+        clicker.gold -= clicker.upgrades[id].price;
+        clicker.upgrades[id].amount += 1;
+        clicker.goldPerSecond += clicker.upgrades[id].gps * 20;
+        clicker.upgrades[id].price = Math.ceil(clicker.upgrades[id].price * clicker.upgrades[id].priceGrowth);
+
+        updateUI(id);
+    }
+}
+
+function gameReset() {
+
+    clicker = createClickerDefault();
+
+    for (i in clicker.upgrades) {
+
+        updateUI(i);
+        clicker.upgrades[i].unlocked = false;
+    }
+}
+
+function saveGame() {
+
+    delay++;
+    if (delay >= 40) {
+
+        Cookies.set("clicker", JSON.stringify(clicker), { expires: 100000 });
+        delay = 0;
+    }
+}
+
+function loadGame() {
+
+    if (Cookies.get("clicker") != null && Cookies.get("clicker") != undefined) {
+
+        var clickerCompare = JSON.parse(Cookies.get("clicker"));
+
+        for (i in clicker.upgrades) {
+
+            if (clickerCompare.upgrades[i] == null || clickerCompare.upgrades[i] == undefined) {
+
+
+                clickerCompare.upgrades[i] = clicker.upgrades[i];
+            }
+        }
+
+        clicker = clickerCompare;
+    }
+    for (i in clicker.upgrades) {
+
+        updateUI(i); /* Make sure UI element's info gets updates every time you load the game */
+        if (clicker.upgrades[i].unlocked) { /* Make sure opacity class is taken out of upgrades dom element every time you load the game */
+
+            var domRef = document.getElementById(clicker.upgrades[i].domRef);
+            domRef.classList.remove("upgrade-opacity");
+        }
+    }
+}
+
+function unlockUpgrades() {
+
+    for (i in clicker.upgrades) {
+
+        var domRef = document.getElementById(clicker.upgrades[i].domRef);
+
+        if (clicker.gold >= clicker.upgrades[i].price && !clicker.upgrades[i].unlocked) {
+
+            domRef.classList.remove("upgrade-opacity");
+            console.log(domRef);
+            clicker.upgrades[i].unlocked = true;
+        } else if (!clicker.upgrades[i].unlocked && clicker.gold < clicker.upgrades[i].price) {
+
+            domRef.classList.add("upgrade-opacity");
+        }
+    }
+}
+
+function gameUpdate() {
+
+    setInterval(() => {
+
+        goldUpdate();
+        unlockUpgrades();
+        saveGame();
+        checkAchievements();
+    }, 50);
+
+    loadGame();
+}
